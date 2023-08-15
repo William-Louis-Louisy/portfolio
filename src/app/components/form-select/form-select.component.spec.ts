506d57b06bcd0d4bb7495d60415f638b4bf104f3
William-Louis-Louisy <williamlouislouisy@yahoo.fr>
2023-08-15 15:28:36 +0200

    Form select created

---
+++ src/app/components/form-select/form-select.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FormSelectComponent } from './form-select.component';
+
+describe('FormSelectComponent', () => {
+  let component: FormSelectComponent;
+  let fixture: ComponentFixture<FormSelectComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [FormSelectComponent]
+    });
+    fixture = TestBed.createComponent(FormSelectComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
